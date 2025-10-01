b1257f0ffe671c994dba676c66c2ffd2f08eb09b
sudheer suraparaju <suraparajusudheer@gmail.com>
2025-10-01 14:23:00 +0530

    2. Added health and products end points

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+products = [
+    {"id":5347, "name":"Garcinea plant", "price":"$5"},
+    {"id":5322, "name":"Chamelia plant", "price":"$4"},
+]
+
 @app.get("/")
 def read_root():
     return {"message":"GreenCart API is running"}
+
+@app.get("/health")
+def read_health():
+    return {"status":"OK"}
+
+@app.get("/products")
+def fetch_products():
+    return {"products":products}
