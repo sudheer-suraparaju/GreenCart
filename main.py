5c3191b37d6af31047a573b6f1146211eecc3999
sudheer suraparaju <suraparajusudheer@gmail.com>
2025-10-02 11:11:35 +0530

    3. Added CORS middleware and id fetch

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # React dev server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 products = [
     {"id":5347, "name":"Garcinea plant", "price":"$5"},
     {"id":5322, "name":"Chamelia plant", "price":"$4"},
@@ -18,3 +29,11 @@ def read_health():
 @app.get("/products")
 def fetch_products():
     return {"products":products}
+
+
+@app.get("/products/{product_id}")
+def fetch_product(product_id: int):
+    product = products.get(product_id)
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return {"product": product}
