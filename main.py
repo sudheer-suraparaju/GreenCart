a3f75f0f4db88d87d4a53548353c2eec8e711804
sudheer suraparaju <suraparajusudheer@gmail.com>
2025-10-09 18:06:19 +0530

    5. Added updating the product by id feature

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class Product(BaseModel):
     id: int
     name: str
@@ -21,21 +22,24 @@ class Product(BaseModel):
 
 
 products = [
-    {"id":5347, "name":"Garcinea plant", "price":"$5"},
-    {"id":5322, "name":"Chamelia plant", "price":"$4"},
+    {"id": 5347, "name": "Garcinea plant", "price": "$5"},
+    {"id": 5322, "name": "Chamelia plant", "price": "$4"},
 ]
+
 
 @app.get("/")
 def read_root():
-    return {"message":"GreenCart API is running"}
+    return {"message": "GreenCart API is running"}
+
 
 @app.get("/health")
 def read_health():
-    return {"status":"OK"}
+    return {"status": "OK"}
+
 
 @app.get("/products")
 def fetch_products():
-    return {"products":products}
+    return {"products": products}
 
 
 @app.get("/products/{product_id}")
@@ -44,6 +48,25 @@ def fetch_product(product_id: int):
     if not product:
         raise HTTPException(status_code=404, detail="Product not found")
     return {"product": product}
+
+
+@app.put("/products/{product_id}")
+def update_product(product_id: int, updated_product: Product):
+    for index, product in enumerate(products):
+        # enumerate to get index for updating the list
+        # enumerate returns (index, value)
+        # value is the product in this case
+        # for example, if product_id is 5347, then
+        # index will be 0 and product will be {"id": 5347, "name": "Garcinea plant", "price": "$5"}
+        if product["id"] == product_id:
+            products[index] = updated_product.dict()
+            # .dict because updated_product is a Pydantic model
+            return {
+                "message": "Product updated successfully",
+                "product": updated_product,
+            }
+    raise HTTPException(status_code=404, detail="Product not found")
+
 
 @app.post("/products")
 def add_product(product: Product):
